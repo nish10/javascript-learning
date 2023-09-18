8056b1734c3f6f9eed292f91e5fe28ea5bc0f18e
Nishant <nishant.raj334@gmail.com>
2023-09-18 23:16:02 +0530

    date object methods

---
+++ 01-variables-data-types/15-date-object-methods/script.js
@@ -0,0 +1,45 @@
+let x;
+let d = new Date();
+
+x = d.toString();
+
+x = d.getTime();
+x = d.valueOf();
+
+x = d.getFullYear();
+x = d.getMonth() + 1; // zero based hence we need to add 1
+
+x = d.getDate();
+
+x = d.getDay();
+
+x = d.getHours();
+
+x = d.getMinutes();
+
+x = d.getSeconds();
+
+x = d.getMilliseconds();
+
+x = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+
+x = Intl.DateTimeFormat('en-US').format(d);
+x = Intl.DateTimeFormat('en-IN').format(d);
+x = Intl.DateTimeFormat('default').format(d);
+
+x = Intl.DateTimeFormat('default', { month: 'long' }).format(d);
+
+x = d.toLocaleString('default', { month: 'short' });
+
+x = d.toLocaleString('default', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  timeZone: 'Asia/Kolkata',
+});
+
+console.log(x);
