e3cdb68778e92dcb598384f6ca64902c1fdc6119
Nishant <nishant.raj334@gmail.com>
2023-09-18 16:25:46 +0530

    comments and shortcuts

---
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,42 @@
+// This is a single line of code
+
+// console.log('comment');
+
+// console.log('multi line comment');
+
+// console.log('multi line comment');
+
+// console.log('multi line comment');
+
+/*
+console.log(100);
+
+console.log('Hello World');
+
+console.log(20, 'Hello', true);
+*/
+
+// const x = 100;
+// console.log(x);
+
+// console.error('Alert');
+/*
+// Multi line comments
+console.warn('Warning');
+
+console.table({ name: 'Nish', email: 'nishant.raj334@gmail.com' });
+*/
+
+console.table({ name: 'Nish', email: 'nishant.raj334@gmail.com' });
+
+console.group('simple');
+console.log(x);
+
+codknsole.error('Alert');
+
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: black; color: green';
+
+console.log('%cHello World', styles);
