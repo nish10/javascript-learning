a2bcb8efa9eb80149d6da19460439d6968039102
Nishant <nishant.raj334@gmail.com>
2023-09-18 21:57:42 +0530

    numbers

---
+++ 01-variables-data-types/11-numbers/script.js
@@ -0,0 +1,23 @@
+let x;
+
+// const num = 5;
+const num = new Number(5);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = Number(num.toFixed(2));
+
+x = num.toPrecision(3);
+
+x = num.toExponential(2);
+
+x = num.toLocaleString('en-US');
+
+x = num.valueOf();
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+console.log(x, typeof x);
