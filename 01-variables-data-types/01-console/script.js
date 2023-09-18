28ffb5009fc7b4774d944a1cd2a398637c9ae1f0
Nishant <nishant.raj334@gmail.com>
2023-09-18 15:21:31 +0530

    consoles

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,26 @@
+console.log(100);
+
+console.log('Hello World');
+
+console.log(20, 'Hello', true);
+
+const x = 100;
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({ name: 'Nish', email: 'nishant.raj334@gmail.com' });
+
+console.group('simple');
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: black; color: green';
+
+console.log('%cHello World', styles);
