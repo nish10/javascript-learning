70f3822b956e4f904f80e653d54773a14c53936c
Nishant <nishant.raj334@gmail.com>
2023-09-18 22:05:23 +0530

    math object

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,22 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.7);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(4.8);
+
+x = Math.pow(2, 3);
+
+x = Math.min(4, 5, 3, 2);
+x = Math.max(2, 3, 5, 6);
+
+x = Math.random();
+
+x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
